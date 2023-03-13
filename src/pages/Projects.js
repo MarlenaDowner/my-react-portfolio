f043b480d578d0a0a2dafaba5fe0fb53bb899d66
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-03-13 23:53:28 +0000

    added projects css

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,39 @@
 import React from "react";
-import img from "../assets/pictures/Horiseon.JPG"; 
+import '../components/style/header.css';
+
 
 
 const Projects = () => {
-    return <div>
-        <h1>hello</h1>
+    return <div className="body2">
+
+   
+    
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Hello, world!</h1>
+        <p>I'm a jumbotron!</p>
+        <p>
+          <button className="btn btn-primary btn-lg">Learn more</button>
+        </p>
+      </div>
+      <div className="card mb-4">
+        <div className="card-header">
+          <h3>Card title</h3>
+        </div>
+        <div className="card-body">
+          <p className="card-text">Card content</p>
+        </div>
+      </div>
+      <div className="card mb-4">
+        <div className="card-header">
+          <h3>Card title</h3>
+        </div>
+        <div className="card-body">
+          <p className="card-text">Card content</p>
+        </div>
+      </div>
+    </div>
+
     </div>
 
 
