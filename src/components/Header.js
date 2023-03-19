a5e07e27bdf2adecb6f82d5eca5246962ec4c45e
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-03-19 13:15:49 +0000

    messed up the background image trying to get the header to the right

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const CustomNavbar = () => {
     return (
         <nav className="navbar navbar-expand-lg">
@@ -32,12 +31,6 @@ const CustomNavbar = () => {
                 </span>
             </div>
         </nav>
-
-
-
-
-
-
     );
 };
 
