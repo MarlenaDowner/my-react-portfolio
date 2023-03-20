c85875d9a031b6233bbedd40ec7336e694adcf88
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-03-20 14:06:54 +0000

    amended route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
-import Header from './components/Header';
+
 
 
 
@@ -18,7 +16,7 @@ function App() {
       <div className="body1">
       {/* <Header style={{ backgroundColor: backgroundColor }} /> */}
 
-        <Header/>
+        {/* <Header/> */}
 
 
         <Routes>
